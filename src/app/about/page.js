e5af93b826e5d83e1d7e8710b471ce94d948b2ce
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-02-29 11:26:03 +0000

    next week

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function page() {
+
+    return (
+        <>
+            <h1>HELLO</h1>
+        </>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { Inter, Gluten, ABeeZee } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const gluten = Gluten({ subsets: ["latin"] });
+const abeezee = ABeeZee({ 
+  subsets: ["latin"],
+  weight: '400'
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={abeezee.className}>{children}</body>
     </html>
   );
 }
